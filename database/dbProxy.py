13c95246bae7b032980163ba62804024c19cf6a7
mrxak <salonf@googlemail.com>
2021-11-08 15:41:07 +0300

    additional helper for getting calibration curves test of getter

--- database/dbProxy.py
+++ database/dbProxy.py
@@ -27,3 +27,25 @@ def getAllLotsList(collection):
     @type collection: pymongo.collection.Collection
     '''
     return collection.find().distinct('ebtLotNo')
+
+def getData4CalibrationCurve(collection, facility='', ebtLotNo='', hoursAfterIrrad=''):
+    '''
+    @type collection: pymongo.collection.Collection
+    '''
+    cs = collection.find({'facilityIdentifier': facility, 'ebtLotNo': ebtLotNo, 'hoursAfterIrrad': hoursAfterIrrad,
+                            'dose': {'$gt': 0}}).sort('dose')
+    ret = {}
+    for i in cs:
+        ret[i['dose']] = i['log10meanMinusZeroFilm']
+    return ret
+
+def getData4CalibrationCurveWithDoseHighLimit(collection, facility='', ebtLotNo='', hoursAfterIrrad='', doseLimit=0.0):
+    '''
+    @type collection: pymongo.collection.Collection
+    '''
+    cs = collection.find({'facilityIdentifier': facility, 'ebtLotNo': ebtLotNo, 'hoursAfterIrrad': hoursAfterIrrad,
+                            'dose': {'$gt': 0.0, '$lt': doseLimit}}).sort('dose')
+    ret = {}
+    for i in cs:
+        ret[i['dose']] = i['log10meanMinusZeroFilm']
+    return ret
