026617ae48415bb0222d36071d9e330c404da5f9
MrXaK <salonf@googlemail.com>
2022-02-07 20:00:31 +0300

    logic of evaluation

--- database/parseCurveToPopt.py
+++ database/parseCurveToPopt.py
@@ -4,6 +4,9 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 import numpy as np
 from scipy.optimize import curve_fit
+from scipy.interpolate import interp1d
+from scipy.interpolate import splrep, splev
+import matplotlib.pyplot as plt
 
 client = MongoClient('mongodb://10.1.30.32:27017/')
 db = client['EBT_films_dose']
@@ -23,6 +26,30 @@ def fit_func(od, a, b, c):
     """
     return (b / (od - a)) + c
 
+def fit_func2(od, a, b, c, d, e):
+    func = np.poly1d([a,b,c, d, e])
+    return func(od)
+
+x = []
+y = []
 s = collectionTifProvider.find(tg).sort('dose')
 for i in s:
     print(i)
+    x.append(i['dose'])
+    y.append(i['log10meanMinusZeroFilm'])
+
+p_opt, p_cov = curve_fit(fit_func2, x, y)
+
+f1 = interp1d(x, y)
+
+f2a = splrep(x, y, s=0)
+f2 = splev(np.linspace(0.0, 21.0, 1000), f2a, der=0)
+
+plt.plot(x, y, "*")
+plt.plot(np.linspace(0.0, 25.0, 1000), fit_func2(np.linspace(0.0, 25.0, 1000), *p_opt))
+
+plt.plot(np.linspace(0.0, 21.0, 1000), f1(np.linspace(0.0, 21.0, 1000)))
+plt.plot(np.linspace(0.0, 21.0, 1000), f2)
+
+
+plt.show()
