92aa642b5b809878f823d276c98685be750e3939
cobaltCorsair <cobaltcorsair@yandex.ru>
2022-03-01 15:41:55 +0300

    Method of using an .ini file

--- database/db_connection.py
+++ database/db_connection.py
@@ -1,12 +1,37 @@
 from pymongo import MongoClient
+from backports import configparser
+import os
+
+path = r'db_config.ini'
 
 
 class Connect:
-
+    """
+    Connecting to the database
+    """
     @staticmethod
     def start():
-        client = MongoClient('mongodb://10.1.30.32:27017/')
-        db = client['EBT_films_dose']
-        collectionTifProvider = db['tifProvider']
+        parameters = Connect.read_config(path)
+
+        client = MongoClient(parameters[0])
+        db = client[parameters[1]]
+        collectionTifProvider = db[parameters[2]]
 
         return collectionTifProvider
+
+    @staticmethod
+    def read_config(path):
+        """
+        Read config
+        """
+        if not os.path.exists(path):
+            return False
+
+        config = configparser.ConfigParser()
+        config.read(path)
+
+        ip_address = config.get("Settings", "client")
+        db = config.get("Settings", "db")
+        collection_tif_provider = config.get("Settings", "tifProvider")
+
+        return [ip_address, db, collection_tif_provider]
