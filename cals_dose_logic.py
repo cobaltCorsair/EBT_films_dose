64c161815a1b42ec3fbcd0e186097c29192895de
cobaltCorsair <cobaltcorsair@yandex.ru>
2023-03-24 12:33:23 +0300

    Minor fixes (comments)

--- cals_dose_logic.py
+++ cals_dose_logic.py
@@ -496,24 +496,30 @@ class CurveWindow(QtWidgets.QWidget, Curve_form):
 
 
 class MoveGraphLine:
+    """
+    Adds functionality to move graphs along the X-axis.
+    """
     def __init__(self, graf, ax, move_speed=0.5):
-        self.graf = graf
-        self.ax = ax
-        self.figcanvas = self.ax.figure.canvas
-        self.start_point = None
-        self.move_speed = move_speed
-        self.moving = False
+        self.graf = graf  # The graph to be moved
+        self.ax = ax  # The axes object of the graph
+        self.figcanvas = self.ax.figure.canvas  # The canvas of the figure
+        self.start_point = None  # The starting point of the mouse drag
+        self.move_speed = move_speed  # The speed of the graph movement
+        self.moving = False  # Flag indicating whether the graph is moving or not
 
+        # Connect the mouse events to their respective handlers
         self.figcanvas.mpl_connect('button_press_event', self.mouse_press)
         self.figcanvas.mpl_connect('button_release_event', self.mouse_release)
         self.figcanvas.mpl_connect('motion_notify_event', self.mouse_move)
 
     def mouse_release(self, event):
+        # Check if a navigation tool is active, if so, do nothing
         if self.ax.get_navigate_mode() is not None:
             return
         self.moving = False
 
     def mouse_press(self, event):
+        # Check if a navigation tool is active, if so, do nothing
         if self.ax.get_navigate_mode() is not None:
             return
         if event.inaxes != self.ax:
@@ -522,6 +528,7 @@ class MoveGraphLine:
         self.moving = True
 
     def mouse_move(self, event):
+        # Check if a navigation tool is active, if so, do nothing
         if self.ax.get_navigate_mode() is not None:
             return
         if event.inaxes != self.ax:
@@ -529,10 +536,12 @@ class MoveGraphLine:
         if not self.moving:
             return
 
+        # Calculate the shift in the x-axis based on the mouse movement
         shift_x = (self.start_point[0] - event.xdata) * self.move_speed
-        mvdx, mvdy = self.graf.get_data()
-        mvdx -= shift_x
+        mvdx, mvdy = self.graf.get_data()  # Get the current graph data
+        mvdx -= shift_x  # Update the x-axis data by applying the shift
 
+        # Update the graph with the new data and redraw the canvas
         self.graf.set_data(mvdx, mvdy)
         self.figcanvas.draw()
 
@@ -635,6 +644,7 @@ class AxesWindow(QtWidgets.QWidget, Axes_form):
         plt.close(self.figure_map_x)
         plt.close(self.figure_map_y)
         self.closeDialog.emit()
+
 
 class CalcUI(QtWidgets.QMainWindow):
     """
