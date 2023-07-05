ba053924e5266659e4cf85d8dd3928620dea96c7
mr-xak <salonf@mrrc.obninsk.ru>
2023-07-05 20:50:55 +0300

    added __init__ for submodule

---
+++ ui/__init__.py
@@ -0,0 +1,13 @@
+__all__ = [
+    "Axes",
+    "calibrate_list",
+    "Curve",
+    "DB_and_settings.py",
+    "Values",
+    ]
+
+from .Axes import *
+from .calibrate_list import *
+from .Curve import *
+from .DB_and_settings import *
+from .Values import *
