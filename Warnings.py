33fd5285efa6372b0e3fcfd907880e08e53791fe
mr-xak <salonf@mrrc.obninsk.ru>
2023-07-08 11:47:05 +0300

    investigating two cases of possible warnings case

--- Warnings.py
+++ Warnings.py
@@ -81,18 +81,19 @@ class Warnings:
     def error_database_is_empty():
         QMessageBox.information(None, "Data error", "<b>Your database connections seems down...</b><br><br>"
                                                  "Seems that we cannot access your valid Mongo database"
-                                                 "We are waiting generic network error time (aboud 30s) and if we "
+                                                 "We are waiting generic network error time (about 30s) and if we "
                                                  "do not see your database is up, this button will be disabled for"
                                                  "the application lifetime."
                                                  "Ignore this if you intenionally want to use this program locally.",
                                 QMessageBox.Ok)
         
     @staticmethod
-    def error_database_is_empty():
+    def error_database_is_empty_readyfallback():
         QMessageBox.information(None, "Data error", "<b>Your database connections seems down...</b><br><br>"
                                                  "Seems that we cannot access your valid Mongo database<br><br>"
                                                  "We are waiting or have been waited generic network error time "
-                                                 " (aboud 30s) and if we do not see your database is up, this button <br>"
+                                                 " (about 30s) and if we do not see your database is up, this button <br>"
                                                  " will be disabled for the application lifetime. <br><br>"
-                                                 "Ignore this if you intenionally want to use this program locally. <br>If you see this in eligible machine, please contact the developers<br><br>",
+                                                 "Ignore this if you intenionally want to use this program locally. <br>"
+                                                 "If you see this in eligible machine, please contact the developers<br><br>",
                                 QMessageBox.Ok)        
